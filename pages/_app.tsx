0015bc11810d05bd79a6272483addd7f5e24f2a4
SeoJunhwan <seojunhwan@kakao.com>
2022-05-02 17:14:39 +0900

    WE-20 메인-1 페이지 마크업, styled-components 이슈 해결
    메인-1 페이지 마크업 완료합니다.
    ----------
    theme, globalStyle 적용
    ----------
    next.js 의 styled-components className 이슈 해결합니다.
    next.config 에서 컴파일러 옵션에 styled-components 추가합니다.
    [내용](https://nextjs.org/docs/advanced-features/compiler#styled-components)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import type { AppProps } from 'next/app';
-import '../styles/globals.css';
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../styles/global';
+import '../styles/fonts.css';
+import { darkTheme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <RecoilRoot>
+        <ThemeProvider theme={darkTheme}>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </RecoilRoot>
+    </>
+  );
 }
 
 export default MyApp;
