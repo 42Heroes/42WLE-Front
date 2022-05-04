87a688dbf70d6a29f80acdcfb6b347f19345db33
SeoJunhwan <seojunhwan@kakao.com>
2022-05-04 10:24:06 +0900

    Merge Develop
    WE-20 Layout 생성
    
    사용하는 레이아웃이 2개라 page 별로 레이아웃 지정할 수 있게끔 설정합니다.
    
    WE-102 언어별 국기 & 언어이름 데이터 생성
    
    WE-102 언어별 국기 & 언어이름 데이터 생성(languages.js 디렉토리 변경)
    
    WE-20 | Button 컴포넌트 생성
    
    공통으로 사용할 버튼 컴포넌트 생성 완료합니다.
    
    props 은 아래와 같습니다.
    
    type: 버튼의 타입 ex) submit
    size: 버튼의 크기 ex) small
    color?: 버튼의 컬러, 기본적으로 생성한 팔레트 색상 사용합니다.
    outline?: 아웃라인에 컬러가 들어가며 바디 색상은 없습니다.
    fullWidth?: 가로폭을 부모 태그에 맞춥니다.
    onClick?: 버튼 클릭시 동작할 함수입니다.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,28 @@
+import '../styles/fonts.css';
+import { NextPage } from 'next';
+import { ReactElement, ReactNode } from 'react';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
-import GlobalStyle from '../styles/global';
-import '../styles/fonts.css';
 import { darkTheme } from '../styles/theme';
+import GlobalStyle from '../styles/global';
 
-function MyApp({ Component, pageProps }: AppProps) {
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
   return (
     <>
       <RecoilRoot>
         <ThemeProvider theme={darkTheme}>
           <GlobalStyle />
-          <Component {...pageProps} />
+          {getLayout(<Component {...pageProps} />)}
         </ThemeProvider>
       </RecoilRoot>
     </>
