99ff1113e2706bb7d8dc94a06375bec7acb017e0
Sara-Jo <sr930927@gmail.com>
2022-05-05 11:08:54 +0900

    WE-98 언어 드롭다운 검색 기능 추가
    issues: WE-24

--- components/common/LanguageDropdown.tsx
+++ components/common/LanguageDropdown.tsx
@@ -1,27 +1,72 @@
+import { ChangeEvent, useState } from 'react';
 import styled from 'styled-components';
 import languages from '../../library/languages';
 import Image from 'next/image';
 
+interface Props {
+  flag: string;
+  language: string;
+}
+
 export default function LanguageDropdown() {
+  const [toggle, setToggle] = useState(false);
+  const [inputText, setInputText] = useState('');
+  const [searchedItems, setSearchedItems] = useState([
+    { flag: '', language: '' },
+  ]);
+  const handleInputText = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+    setSearchedItems(
+      languages.filter((item: Props) =>
+        item.language.toLowerCase().includes(inputText.toLowerCase()),
+      ),
+    );
+  };
   return (
-    <Container>
-      <Input type="text" placeholder="Enter your language" />
-      <ul>
-        {languages.map((language) => (
-          <LanguageBox key={language.language}>
-            <Image
-              alt={language.language}
-              src={language.flag}
-              width={18}
-              height={11}
-            />
-            <List>{language.language}</List>
-          </LanguageBox>
-        ))}
-      </ul>
-    </Container>
+    <div>
+      <PlusButton onClick={() => setToggle(!toggle)}>+</PlusButton>
+      {toggle && (
+        <Container>
+          <Input
+            value={inputText}
+            onChange={handleInputText}
+            type="text"
+            placeholder="Enter your language"
+          />
+          <ul>
+            {inputText
+              ? searchedItems.map((item: Props) => (
+                  <LanguageBox key={item.language}>
+                    <Image
+                      alt={item.language}
+                      src={item.flag}
+                      width={18}
+                      height={11}
+                    />
+                    <List>{item.language}</List>
+                  </LanguageBox>
+                ))
+              : languages.map((language: Props) => (
+                  <LanguageBox key={language.language}>
+                    <Image
+                      alt={language.language}
+                      src={language.flag}
+                      width={18}
+                      height={11}
+                    />
+                    <List>{language.language}</List>
+                  </LanguageBox>
+                ))}
+          </ul>
+        </Container>
+      )}
+    </div>
   );
 }
+
+const PlusButton = styled.button`
+  color: white;
+`;
 
 const Container = styled.div`
   width: 17rem;
