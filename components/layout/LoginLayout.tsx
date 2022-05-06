07dde28ae1aa14777b258122f2c7e88767f3384c
SeoJunhwan <seojunhwan@kakao.com>
2022-05-06 17:27:59 +0900

    WE-24 Login-Layout 수정
    기존 LoginLayout 의 경우 높이만 100vh 로 주었지, display:flex 적용이 안 되어있어서
    수평 정렬은 되어있으나 수직 정렬이 안 되어있어서 수정합니다!

--- components/layout/LoginLayout.tsx
+++ components/layout/LoginLayout.tsx
@@ -13,4 +13,8 @@ const Container = styled.div`
   max-width: 1200px;
   margin: 0 auto;
   height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
