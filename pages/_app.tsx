3d661f695aa583b58454431b6cdab8925a483c3e
SeoJunhwan <seojunhwan@kakao.com>
2022-05-08 18:03:21 +0900

    WE-111 MSW 활성 조건 변경
    기존 개발 환경 활성 => 환경변수 NEXT_PUBLIC_API_MOCKING === enabled 일 때 활성되도록
    조건 변경합니다.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
   require('../mocks');
 }
 
