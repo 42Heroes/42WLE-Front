fcdf1613fbb9973524c1f0d9f4369cf85b7a80c1
SeoJunhwan <seojunhwan@kakao.com>
2022-05-08 17:56:18 +0900

    WE-111 MSW 로딩 로직 이동
    _app 파일이 더러워지고 굳이 노출될 필요가 없는 정보라 이동합니다.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,7 @@ import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
 if (process.env.NODE_ENV === 'development') {
-  if (typeof window === 'undefined') {
-    (async () => {
-      const { server } = await import('../mocks/node');
-      server.listen();
-    })();
-  } else {
-    (async () => {
-      const { worker } = await import('../mocks/browser');
-      worker.start();
-    })();
-  }
+  require('../mocks');
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
