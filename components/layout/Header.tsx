a24483c022826bd7aa251a53a5c90a63dd2076c7
Sara-Jo <sr930927@gmail.com>
2022-05-10 20:40:03 +0900

    WE-112 페이지 이동에 따라 navbar 아이콘 활성화표시, header 텍스트 변경

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,10 +1,27 @@
+import { useRouter } from 'next/router';
 import styled from 'styled-components';
 
+function getHeaderText(pathname: string) {
+  if (pathname === '/find') return 'Find';
+  if (pathname === '/chat') return 'Chat';
+  if (pathname === '/board') return 'Board';
+  if (pathname === '/mypage') return 'My Page';
+}
+
 export default function Header() {
-  return <Container></Container>;
+  const router = useRouter();
+  const headerText = getHeaderText(router.pathname);
+
+  return <Container>{headerText}</Container>;
 }
 
 const Container = styled.header`
   margin-left: -1px;
   border-bottom: 1px solid ${({ theme }) => theme.grayColor};
+  ${({ theme }) => theme.font.subTitleBold};
+  color: ${({ theme }) => theme.fontColor.titleColor};
+  display: flex;
+  align-items: center;
+  padding-left: 3rem;
+  white-space: nowrap;
 `;
--- components/layout/Nav.tsx
+++ components/layout/Nav.tsx
@@ -4,26 +4,40 @@ import LibraryBooksRoundedIcon from '@mui/icons-material/LibraryBooksRounded';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
 import styled from 'styled-components';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+interface Prop {
+  isActive: boolean;
+}
 
 export default function Nav() {
+  const router = useRouter();
+
   return (
     <Container>
       <UpperNav>
-        <IconContainer>
-          <PersonAddAltOutlinedIcon sx={{ fontSize: 25 }} />
-        </IconContainer>
-        <IconContainer>
-          <ChatRoundedIcon sx={{ fontSize: 25 }} />
-        </IconContainer>
-        <IconContainer>
-          <LibraryBooksRoundedIcon sx={{ fontSize: 25 }} />
-        </IconContainer>
+        <Link href="/find" passHref>
+          <IconContainer isActive={router.pathname === '/find' ? true : false}>
+            <PersonAddAltOutlinedIcon sx={{ fontSize: 25 }} />
+          </IconContainer>
+        </Link>
+        <Link href="/chat" passHref>
+          <IconContainer isActive={router.pathname === '/chat' ? true : false}>
+            <ChatRoundedIcon sx={{ fontSize: 25 }} />
+          </IconContainer>
+        </Link>
+        <Link href="/board" passHref>
+          <IconContainer isActive={router.pathname === '/board' ? true : false}>
+            <LibraryBooksRoundedIcon sx={{ fontSize: 25 }} />
+          </IconContainer>
+        </Link>
       </UpperNav>
       <LowerNav>
-        <IconContainer>
+        <IconContainer isActive={router.pathname === '/mypage' ? true : false}>
           <AccountCircleOutlinedIcon sx={{ fontSize: 25 }} />
         </IconContainer>
-        <IconContainer>
+        <IconContainer isActive={false}>
           <ExitToAppOutlinedIcon sx={{ fontSize: 25 }} />
         </IconContainer>
       </LowerNav>
@@ -37,10 +51,16 @@ const Container = styled.nav`
   border-right: 1px solid ${({ theme }) => theme.grayColor};
 `;
 
-const IconContainer = styled.div`
-  color: ${({ theme }) => theme.fontColor.contentColor};
+const IconContainer = styled.div<Prop>`
+  color: ${(props) =>
+    props.isActive
+      ? ({ theme }) => theme.pointColor
+      : ({ theme }) => theme.fontColor.contentColor};
+  background-color: ${(props) => (props.isActive ? '#242526' : 'none')};
   margin-top: 3rem;
   padding: 1rem 1.5rem;
+  border-radius: 1rem;
+  cursor: pointer;
 `;
 
 const UpperNav = styled.div`
