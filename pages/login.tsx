1b12f8b0b7ac14b19e6adf44d6c508d82756813f
SeoJunhwan <seojunhwan@kakao.com>
2022-05-10 13:22:36 +0900

    WE-113 Login 페이지 수정 완료
    CSS 수정 완료
    oAuth 버튼 onClick 함수 생성

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,13 +4,21 @@ import { ReactElement } from 'react';
 import LoginLayout from '../components/layout/LoginLayout';
 import FTLogo from '../components/common/FTLogo';
 export default function Login() {
+  const handleClickLoginButton = () => {
+    // TODO: oAuth 로그인 로직
+  };
   return (
     <Container>
       <MainMsg>
         <h1>Log in</h1>
         <h2>with just one click!</h2>
       </MainMsg>
-      <StyledButton type="button" size="large" color="blue">
+      <StyledButton
+        type="button"
+        size="large"
+        color="blue"
+        onClick={handleClickLoginButton}
+      >
         <FTLogo width="40" />
         Sign In with 42 Intra
       </StyledButton>
@@ -24,34 +32,32 @@ Login.getLayout = function getLayout(page: ReactElement) {
 
 const Container = styled.div`
   display: flex;
-  height: 100%;
-  margin: 0 auto;
   flex-direction: column;
   align-items: center;
   justify-content: center;
 `;
+
 const MainMsg = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-bottom: 9rem;
+  line-height: 9rem;
+  text-align: center;
+  color: ${({ theme }) => theme.fontColor.titleColor};
   h1 {
     font-size: ${({ theme }) => theme.font.headingBold};
   }
   h2 {
     font-size: ${({ theme }) => theme.font.subTitleBold};
   }
-  color: ${({ theme }) => theme.fontColor.titleColor};
-  display: flex;
-  margin: 0 auto;
-  flex-direction: column;
-  justify-content: center;
-  text-align: center;
-  line-height: 9rem;
 `;
 
 const StyledButton = styled(Button)`
   background-color: ${({ theme }) => theme.pointColor};
   border-radius: 1rem;
-  margin-top: 5rem;
+  padding: 3rem;
   svg {
     margin-right: 1rem;
   }
-  padding: 3rem;
 `;
