d4a59e940524650f410803a665171df8842072e9
SeoJunhwan <seojunhwan@kakao.com>
2022-05-11 23:42:46 +0900

    WE-113 Language 컴포넌트 마진 수정

--- components/common/Language.tsx
+++ components/common/Language.tsx
@@ -11,26 +11,34 @@ interface Props {
 export default function Language({ language }: Props) {
   return (
     <Container>
-      <Image
-        alt={language.language}
-        src={language.flag}
-        width={23}
-        height={14}
-      />
+      <FlagWrapper>
+        <Image
+          alt={language.language}
+          src={language.flag}
+          width={23}
+          height={14}
+        />
+      </FlagWrapper>
       <LanguageName>{language.language.toUpperCase()}</LanguageName>
     </Container>
   );
 }
 const LanguageName = styled.span`
-  margin-left: 0.7rem;
   text-transform: uppercase;
   color: ${({ theme }) => theme.grayColor};
+  text-align: center;
+  width: 100%;
+`;
+
+const FlagWrapper = styled.div`
+  display: flex;
+  min-width: 2.3rem;
 `;
 
 const Container = styled.li`
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   padding: 0.3rem 0.8rem;
   border: 0.1rem solid ${({ theme }) => theme.grayColor};
   border-radius: 0.5rem;
