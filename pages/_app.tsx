646c8edcee95b629f6ef7c4b2de7deabd362fb10
SeoJunhwan <seojunhwan@kakao.com>
2022-05-14 21:45:31 +0900

    WE-118 react-query 설정 변경
    기존 설정에서 추후 SSG or SSR 사용시까지 대응하도록 설정 변경합니다.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import '../styles/fonts.css';
 import { NextPage } from 'next';
-import { ReactElement, ReactNode } from 'react';
+import { ReactElement, ReactNode, useState } from 'react';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
 import { darkTheme } from '../styles/theme';
 import GlobalStyle from '../styles/global';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider, Hydrate } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 
 type NextPageWithLayout = NextPage & {
@@ -21,19 +21,21 @@ if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
   require('../mocks');
 }
 
-const queryClient = new QueryClient();
-
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <ThemeProvider theme={darkTheme}>
-          <GlobalStyle />
-          {getLayout(<Component {...pageProps} />)}
-          <ReactQueryDevtools initialIsOpen={false} />
-        </ThemeProvider>
-      </RecoilRoot>
+      <Hydrate state={pageProps.dehydratedState}>
+        <RecoilRoot>
+          <ThemeProvider theme={darkTheme}>
+            <GlobalStyle />
+            {getLayout(<Component {...pageProps} />)}
+            <ReactQueryDevtools initialIsOpen={false} />
+          </ThemeProvider>
+        </RecoilRoot>
+      </Hydrate>
     </QueryClientProvider>
   );
 }
