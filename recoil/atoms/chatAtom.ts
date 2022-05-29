e035c3eb612da6104d0a57a45bcf3473d763dc70
SeoJunhwan <seojunhwan@kakao.com>
2022-05-29 22:53:48 +0900

    WE-23 activeChat atom 추가 및 activeChatRoom Selector 생성
    activeChatRoomIdState 로 현재 활성화된 채팅룸의 id 를 관리합니다.
    
    recoil selector 를 이용하여 activeChatRoomIdState 와 chatState 를 통해 현재 활성화된 채팅방을 관리합니다.
    
    activeChatRoom 를 이용하여 현재 활성화된 채팅방의 상대 즉 activeChatPartnerState 를 관리합니다.

--- recoil/atoms/chatAtom.ts
+++ recoil/atoms/chatAtom.ts
@@ -1,7 +1,12 @@
 import { atom } from 'recoil';
 import { Chat } from '../../interfaces/chat.interface';
 
+export const activeChatRoomIdState = atom<string>({
+  key: 'activeChatRoom',
+  default: '',
+});
+
 export const chatState = atom<Chat[]>({
   key: 'chatState',
-  default: []
+  default: [],
 });
