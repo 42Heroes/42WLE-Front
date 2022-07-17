ea22595285602d97f1c8d5bd78419104576fb0f1
SeoJunhwan <seojunhwan@kakao.com>
2022-07-17 11:54:06 +0900

    WE-170 VideoPlayer 컴포넌트 생성
    로컬 비디오 플레이어 컴포넌트와 리모트 비디오 플레이어 컴포넌트, 이를 제어하는 VideoNav 컴포넌트 생성합니다.

---
+++ components/call/LocalVideo.tsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import VideoNav from './VideoNav';
+
+interface Props {
+  muted?: boolean;
+  autoPlay?: boolean;
+  playsInline?: boolean;
+  videoRef: React.RefObject<HTMLVideoElement>;
+  stream: MediaStream | null;
+  handleEndCall: () => void;
+}
+
+export default function LocalVideo({
+  autoPlay,
+  playsInline,
+  videoRef,
+  stream,
+  handleEndCall,
+}: Props) {
+  const [localCallStatus, setLocalCallStatus] = useState({
+    mute: false,
+    camera: true,
+  });
+
+  const toggleMute = () => {
+    stream?.getAudioTracks().forEach((track) => {
+      track.enabled = !localCallStatus.mute;
+    });
+    setLocalCallStatus({ ...localCallStatus, mute: !localCallStatus.mute });
+  };
+
+  const toggleCamera = () => {
+    stream?.getVideoTracks().forEach((track) => {
+      track.enabled = !localCallStatus.camera;
+    });
+    setLocalCallStatus({ ...localCallStatus, camera: !localCallStatus.camera });
+  };
+
+  return (
+    <Container>
+      <Video ref={videoRef} autoPlay={autoPlay} playsInline={playsInline} />
+      <VideoNav
+        isMuted={localCallStatus.mute}
+        isCameraOn={localCallStatus.camera}
+        onToggleMute={toggleMute}
+        onToggleCamera={toggleCamera}
+        onEndCall={handleEndCall}
+      />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: inline-block;
+  position: relative;
+  overflow: hidden;
+`;
+
+const Video = styled.video`
+  width: 60rem;
+  border-radius: 1rem;
+  transform: rotateY(180deg);
+`;
