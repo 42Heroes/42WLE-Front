6926bc77a27bb3353603fb9f3d5662fec4600f12
Sara-Jo <sr930927@gmail.com>
2022-07-26 00:04:27 +0900

    코멘트 입력창 스타일 수정

--- components/board/Comments.tsx
+++ components/board/Comments.tsx
@@ -2,21 +2,26 @@ import styled from 'styled-components';
 import useInput from '../../hooks/useInput';
 import useMe from '../../hooks/useMe';
 import ProfileImage from '../common/ProfileImage';
+import SendRoundedIcon from '@mui/icons-material/SendRounded';
 
 export default function Comments() {
   const { data: me } = useMe();
   const [value, onChangeInputText, setInputText] = useInput();
+  const SendBtnColor = value.length ? '#8083FF' : '#727272';
   const handleInputKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {};
   return (
     <Container>
       <WriteCommentContainer>
         {me && <ProfileImage src={me.image_url} size="small" />}
-        <input
-          placeholder="Write a comment"
-          value={value}
-          onChange={onChangeInputText}
-          onKeyDown={handleInputKeyDown}
-        />
+        <WriteCommentBox>
+          <input
+            placeholder="Write a comment"
+            value={value}
+            onChange={onChangeInputText}
+            onKeyDown={handleInputKeyDown}
+          />
+          <SendRoundedIcon sx={{ color: SendBtnColor, fontSize: 23 }} />
+        </WriteCommentBox>
       </WriteCommentContainer>
     </Container>
   );
@@ -25,7 +30,7 @@ export default function Comments() {
 const Container = styled.div`
   width: 65rem;
   margin: 0 3rem;
-  padding: 2rem 3rem;
+  padding: 2rem 2.5rem;
   background-color: #242526;
   border-top: 1px solid ${({ theme }) => theme.grayColor};
   border-radius: 0 0 1rem 1rem;
@@ -34,18 +39,32 @@ const Container = styled.div`
 const WriteCommentContainer = styled.div`
   display: flex;
   align-items: center;
+`;
+
+const WriteCommentBox = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  border-radius: 1rem;
+  background-color: #3a3b3c;
+  margin-left: 1rem;
+  padding: 0.5rem 1rem;
   input {
     width: 100%;
-    background-color: #3a3b3c;
     margin-left: 1rem;
+    background-color: inherit;
     color: ${({ theme }) => theme.fontColor.contentColor};
-    border-radius: 1rem;
-    padding: 1rem;
     ::placeholder {
       color: ${({ theme }) => theme.fontColor.contentColor};
     }
     &:focus {
       outline: none;
+    }
+  }
+  svg {
+    &:last-child {
+      transform: rotateZ(-45deg);
+      margin-bottom: 0.5rem;
     }
   }
 `;
