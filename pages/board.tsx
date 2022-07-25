8210ef07bc0e3f0ccfaa0db5b6a6fcd73a72cb41
Sara-Jo <sr930927@gmail.com>
2022-07-25 21:28:07 +0900

    필터링된 유저 없을때 메시지 띄우기

--- pages/board.tsx
+++ pages/board.tsx
@@ -11,7 +11,6 @@ import PostCard from '../components/board/PostCard';
 export default function Board() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const postsData = useQuery<Post[]>('posts', getPosts);
-  console.log(postsData.data);
 
   const toggleModal = (e: React.MouseEvent<HTMLDivElement>) => {
     if (e.defaultPrevented) {
