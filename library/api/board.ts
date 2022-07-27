d1d5a2ba16e8f07b1a4961a1f3ad212941cda263
Sara-Jo <sr930927@gmail.com>
2022-07-27 12:57:07 +0900

    deletePost 기능 완성

--- library/api/board.ts
+++ library/api/board.ts
@@ -1,4 +1,8 @@
-import { CreateComment, CreatePost, Post, UpdatePost } from '../../interfaces/board.interface';
+import {
+  CreateComment,
+  CreatePost,
+  UpdatePost,
+} from '../../interfaces/board.interface';
 import { axiosInstance } from './axios-instance';
 
 export const getPosts = async () => {
@@ -14,25 +18,33 @@ export const getPostById = async (postId: string) => {
 };
 
 export const createPost = async (newPost: CreatePost) => {
-  const { data } = await axiosInstance.post('/board', newPost, {withCredentials: true});
+  const { data } = await axiosInstance.post('/board', newPost);
 
   return data;
 };
 
-export const updatePost = async ({boardId, contents}: UpdatePost) => {
-  const { data } = await axiosInstance.put('/board', {boardId, contents});
+export const updatePost = async ({ boardId, contents }: UpdatePost) => {
+  const { data } = await axiosInstance.put('/board', { boardId, contents });
 
   return data;
 };
 
 export const deletePost = async (boardId: string) => {
-  const { data } = await axiosInstance.delete('/board', {data:boardId});
+  const { data } = await axiosInstance.delete('/board', {
+    data: {
+      boardId,
+    },
+  });
 
   return data;
 };
 
-export const createComment = async ({boardId, content}: CreateComment) => {
-  const { data } = await axiosInstance.post('/board/comment', {boardId, content}, {withCredentials: true});
+export const createComment = async ({ boardId, content }: CreateComment) => {
+  const { data } = await axiosInstance.post(
+    '/board/comment',
+    { boardId, content },
+    { withCredentials: true },
+  );
 
   return data;
 };
