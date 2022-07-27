150947481675cb7acce22c5743780243cddd57a5
SeoJunhwan <seojunhwan@kakao.com>
2022-07-28 03:57:00 +0900

    WE-155 useMe 커스텀 훅 실패시 다시 시도 횟수 0 설정
    다시 시도함에 따라 사용자에게 ui 렌더링이 늦춰지기에 retry false 로 설정합니다.

--- hooks/useMe.ts
+++ hooks/useMe.ts
@@ -1,11 +1,11 @@
 import { useQuery } from 'react-query';
-import { useRecoilState, useSetRecoilState } from 'recoil';
+import { useSetRecoilState } from 'recoil';
 import { User } from '../interfaces/user.interface';
 import { getMe } from '../library/api';
 import { loginState, userState } from '../recoil/atoms';
 
 export default function useMe() {
-  const [isLoggedIn, setIsLoggedIn] = useRecoilState(loginState);
+  const setIsLoggedIn = useSetRecoilState(loginState);
   const setUser = useSetRecoilState(userState);
 
   return useQuery<User>(['user', 'me'], getMe, {
@@ -16,6 +16,6 @@ export default function useMe() {
       setUser(response);
     },
     onError: () => setIsLoggedIn(false),
-    enabled: isLoggedIn,
+    retry: false,
   });
 }
