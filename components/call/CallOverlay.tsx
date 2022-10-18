0d62da53b179706d53a81626326ebdb716fb7ed7
SeoJunhwan <seojunhwan@kakao.com>
2022-10-18 17:47:02 +0900

    WE-258: 채팅방으로 돌아갈 수 있는 버튼 생성
    오버레이에 해당 채팅방으로 돌아갈 수 있는 버튼 생성합니다.

--- components/call/CallOverlay.tsx
+++ components/call/CallOverlay.tsx
@@ -1,14 +1,17 @@
 import styled from 'styled-components';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { callInfoState } from '../../recoil/selectors';
-import ActiveVideoCall from './ActiveVideoCall';
 import { useRouter } from 'next/router';
 import { useEffect, useRef, useState } from 'react';
-import Draggable from 'react-draggable';
+import Draggable, { DraggableData, DraggableEvent } from 'react-draggable';
+import RemoteVideo from './RemoteVideo';
+import { activeChatRoomIdState } from '../../recoil/atoms';
+import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
 
 export default function CallOverlay() {
-  const { isCalling, users } = useRecoilValue(callInfoState);
-
+  const { isCalling, users, roomNo } = useRecoilValue(callInfoState);
+  const setActiveChatRoomId = useSetRecoilState(activeChatRoomIdState);
+  const [isDragging, setIsDragging] = useState(false);
   const [overlayPosition, setOverlayPosition] = useState({
     x: 0,
     y: 0,
@@ -17,6 +20,23 @@ export default function CallOverlay() {
   const router = useRouter();
 
   const ref = useRef(null);
+
+  const handleClickCallTitle = () => {
+    setActiveChatRoomId(roomNo);
+    router.push('/chat');
+  };
+
+  const handleDrag = (e: DraggableEvent, data: DraggableData) => {
+    setOverlayPosition({ x: data.x, y: data.y });
+  };
+
+  const handleDragStart = () => {
+    setIsDragging(true);
+  };
+
+  const handleDragStop = () => {
+    setIsDragging(false);
+  };
 
   useEffect(() => {
     setOverlayPosition({
@@ -30,21 +50,56 @@ export default function CallOverlay() {
       nodeRef={ref}
       bounds="parent"
       position={overlayPosition}
-      onStart={(e, data) => {
-        console.log(e, data);
-      }}
-      onDrag={(e, data) => {
-        setOverlayPosition({ x: data.x, y: data.y });
-      }}
+      onDrag={handleDrag}
+      onStart={handleDragStart}
+      onStop={handleDragStop}
     >
-      <Container ref={ref}>
-        <ActiveVideoCall />
+      <Container ref={ref} isDragging={isDragging}>
+        <VideoTitle onClick={handleClickCallTitle}>
+          {/*TODO: 채팅방 입장 인원들 이름으로 변경*/}
+          <span>
+            <ArrowBackRoundedIcon />
+          </span>
+        </VideoTitle>
+        {users.map((user) => (
+          <RemoteVideo
+            key={user.socketId}
+            stream={user.stream}
+            autoPlay
+            playsInline
+          />
+        ))}
       </Container>
     </Draggable>
   ) : null;
 }
 
-const Container = styled.div`
+const Container = styled.div<{ isDragging: boolean }>`
   position: absolute;
-  width: 30rem;
+  max-width: 300px;
+  transition: filter 0.2s ease-in-out;
+
+  &:hover {
+    filter: ${({ isDragging }) =>
+      isDragging ? 'brightness(0.7)' : 'brightness(0.8)'};
+    cursor: ${({ isDragging }) => (isDragging ? 'grabbing' : 'grab')};
+  }
+`;
+
+const VideoTitle = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 999;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  color: white;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.2);
+  }
 `;
